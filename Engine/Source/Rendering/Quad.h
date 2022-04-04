1378f3549dda1ad234110514073b8f3a49b591ed
Cory Atkinson <attikins13@gmail.com>
2022-04-04 16:35:58 +1000

    Added Quad struct to handle vertex information

---
+++ Engine/Source/Rendering/Quad.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Vertex.h"
+
+#include <vector>
+
+namespace SE {
+	struct Quad {
+		Vertex Vertices[4];
+
+		Quad() = default;
+        Quad(const glm::vec3& position, const glm::vec2& scale) {
+            Vertices[0].Position = position + glm::vec3(-scale.x * 0.5f, -scale.y * 0.5f, 0.0f);
+            Vertices[1].Position = position + glm::vec3(-scale.x * 0.5f,  scale.y * 0.5f, 0.0f);
+            Vertices[2].Position = position + glm::vec3( scale.x * 0.5f,  scale.y * 0.5f, 0.0f);
+            Vertices[3].Position = position + glm::vec3( scale.x * 0.5f, -scale.y * 0.5f, 0.0f);
+
+            Vertices[0].UV = { 0.0f, 0.0f };
+            Vertices[1].UV = { 0.0f, 1.0f };
+            Vertices[2].UV = { 1.0f, 1.0f };
+            Vertices[3].UV = { 1.0f, 0.0f };
+        }
+
+        std::vector<Vertex> ToVertexData() {
+            return std::vector<Vertex>({ Vertices[0], Vertices[1], Vertices[2], Vertices[2], Vertices[3], Vertices[0] });
+        }
+	};
+}
---
+++ Engine/Source/Rendering/Vertex.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <glm/glm.hpp>
+
+namespace SE {
+	struct Vertex {
+		glm::vec3 Position;
+		glm::vec2 UV;
+	};
+}
