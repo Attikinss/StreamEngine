d6c3ae5fb49a40afbd93b3f19c371fb6dad7d067
Cory Atkinson <attikins13@gmail.com>
2022-04-13 20:58:06 +1000

    Component no longer has Update and FixedUpdate

--- Engine/Source/Scene/Components/Component.h
+++ Engine/Source/Scene/Components/Component.h
@@ -1,16 +1,9 @@
 #pragma once
-
 namespace SE {
 	class Component {
-		friend class Entity;
-		friend class Scene;
-
 	public:
 		Component() = default;
 		virtual ~Component() = default;
-
-		virtual void OnUpdate() {}
-		virtual void OnFixedUpdate() {}
 
 	public:
 		bool IsEnabled = true;
