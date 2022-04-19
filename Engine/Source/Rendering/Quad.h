992991e51889718ecb7eed200d6c52e3e7a4a704
Cory Atkinson <attikins13@gmail.com>
2022-04-19 16:56:06 +1000

    Added helper functions for Quad

--- Engine/Source/Rendering/Quad.h
+++ Engine/Source/Rendering/Quad.h
@@ -33,6 +33,34 @@ namespace SE {
 
         }
 
+        void SetTextureID(uint32_t id) {
+            Vertices[0].TextureID = (float)id;
+            Vertices[1].TextureID = (float)id;
+            Vertices[2].TextureID = (float)id;
+            Vertices[3].TextureID = (float)id;
+        }
+
+        void SetColor(const glm::vec4& color) {
+            Vertices[0].Color = color;
+            Vertices[1].Color = color;
+            Vertices[2].Color = color;
+            Vertices[3].Color = color;
+        }
+
+        void FlipX() {
+            Vertices[0].UV.x = 1.0f - Vertices[0].UV.x;
+            Vertices[1].UV.x = 1.0f - Vertices[1].UV.x;
+            Vertices[2].UV.x = 1.0f - Vertices[2].UV.x;
+            Vertices[3].UV.x = 1.0f - Vertices[3].UV.x;
+        }
+
+        void FlipY() {
+            Vertices[0].UV.y = 1.0f - Vertices[0].UV.y;
+            Vertices[1].UV.y = 1.0f - Vertices[1].UV.y;
+            Vertices[2].UV.y = 1.0f - Vertices[2].UV.y;
+            Vertices[3].UV.y = 1.0f - Vertices[3].UV.y;
+        }
+
         std::vector<Vertex> ToVertexData() const {
             return std::vector<Vertex>({ Vertices[0], Vertices[1], Vertices[2], Vertices[2], Vertices[3], Vertices[0] });
         }
